93322af00e3c19df6d17df340abfe85fa559bb0b
長田 凌 <nagata@private.wide.co.jp>
2019-04-08 14:03:18 +0900

    メイン画面が文字化けしちゃう

--- Main.php
+++ Main.php
@@ -1,6 +1,14 @@
 <?php
 session_start();
 
+$string = (string)filter_input(INPUT_POST,$_SESSION['NAME']);
+if ($string){
+    $string = htmlspecialchar($string, ENT_QUOTES,'SJIS-win');
+} else {
+    $string = 'no data.';
+}
+
+
 // ログイン状態チェック
 if (!isset($_SESSION['NAME'])) {
     header("Location: logout.php");
@@ -11,7 +19,7 @@ if (!isset($_SESSION['NAME'])) {
 <!DOCTYPE html>
 <html lang="ja">
     <head>
-        <meta charset="utf-8">
+        <meta charset="shift_JIS">
         <title>メイン</title>
         <link rel= "stylesheet" href="style.css">
         <script src="script.js"></script>
@@ -19,7 +27,7 @@ if (!isset($_SESSION['NAME'])) {
     <body>
         <h1>メイン画面</h1>
         <!-- ユーザーIDにHTMLタグが含まれても良いようにエスケープする -->
-        <p>ようこそ<u><?php echo htmlspecialchars($_SESSION["NAME"], ENT_QUOTES); ?></u>さん</p>  <!-- ユーザー名をechoで表示 -->
+        <p>ようこそ<u><?php echo htmlspecialchars($_SESSION['NAME'], ENT_QUOTES); ?></u>さん</p>  <!-- ユーザー名をechoで表示 -->
         <ul>
             <li><a href="logout.php">ログアウト</a></li>
         </ul>
