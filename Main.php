125ec45141c7ca7c280b7a272354dd47c8d834f8
長田 凌 <nagata@private.wide.co.jp>
2019-04-08 13:43:56 +0900

    ログアウトＰＨＰが足りんと言われ

--- Main.php
+++ Main.php
@@ -3,7 +3,7 @@ session_start();
 
 // ログイン状態チェック
 if (!isset($_SESSION['NAME'])) {
-    header("Location: Logout.php");
+    header("Location: logout.php");
     exit;
 }
 ?>
@@ -19,7 +19,7 @@ if (!isset($_SESSION['NAME'])) {
         <!-- ユーザーIDにHTMLタグが含まれても良いようにエスケープする -->
         <p>ようこそ<u><?php echo htmlspecialchars($_SESSION["NAME"], ENT_QUOTES); ?></u>さん</p>  <!-- ユーザー名をechoで表示 -->
         <ul>
-            <li><a href="Logout.php">ログアウト</a></li>
+            <li><a href="logout.php">ログアウト</a></li>
         </ul>
     </body>
 </html>
