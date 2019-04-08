9b57151c125af4bbab6742a914b43a8a452c40ba
長田 凌 <nagata@private.wide.co.jp>
2019-04-08 11:41:30 +0900

    test

--- index.php
+++ index.php
@@ -4,21 +4,34 @@ session_start();
 
 header('Location: /index.html');
 
+$dbUrl = parse_url(getenv('DATABASE_URL'));
+
+$db['host'] = $dbUrl['host'];  // DBサーバのURL
+$db['user'] = $dbUrl['user'];  // ユーザー名
+$db['pass'] = $dbUrl['pass'];  // ユーザー名のパスワード
+$db['dbname'] = ltrim($dbUrl['path'], '/');;  // データベース名
+
+
 //エラーメッセージの初期化
 $errorMessage = "";
 
-//ログインボタン押下処理
-if(empty($_POST["login_Button"])){
-    if(empty($_POST["username"])){
-        $errorMessage = "ユーザ名が未入力です";
-    }else if(empty($_POST["password"])){
-        $errorMessage = "パスワードが未入力です";
+// ログインボタンが押された場合
+if (isset($_POST["login"])) {
+    // 1. ユーザIDの入力チェック
+    if (empty($_POST["username"])) {  // emptyは値が空のとき
+        $errorMessage = 'ユーザーIDが未入力です。';
+    } else if (empty($_POST["password"])) {
+        $errorMessage = 'パスワードが未入力です。';
     }
-    //ユーザ名もパスも入力されていたら
-//    if(!empty($_POST["username"] && !empty($_POST["password"])){
-        //postgresSQLに接続
-        
-//    }
+    
+    if (!empty($_POST["username"]) && !empty($_POST["password"])) {
+        // 入力したユーザIDを格納
+        $username = $_POST["username"];
+
+        // 2. ユーザIDとパスワードが入力されていたら認証する
+        $dsn = sprintf('pgsql: host=%s; dbname=%s;', $db['host'], $db['dbname']);
+    }
+
 }
 
 ?>
