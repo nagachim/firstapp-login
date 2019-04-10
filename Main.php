8f5cc3a882d163980053509e706ddb11a4d44267
長田 凌 <nagata@private.wide.co.jp>
2019-04-10 16:33:26 +0900

    カウント機能完成？

--- Main.php
+++ Main.php
@@ -8,9 +8,6 @@ if (!isset($_SESSION['nickname'])) {
     exit;
 }
 
-$text = $_SESSION['nickname'];
-$text = mb_convert_encoding($text, 'Shift-JIS', 'UTF-8');
-
 ?>
 
 <!DOCTYPE html>
@@ -23,7 +20,7 @@ $text = mb_convert_encoding($text, 'Shift-JIS', 'UTF-8');
     </head>
     <body>
         <h1>メイン画面</h1>
-        <p>ようこそ<u><?php echo htmlspecialchars($text, ENT_QUOTES,'Shift-JIS'); ?></u>さん</p>
+        <p>ようこそ<u><?php echo htmlspecialchars(mb_convert_encoding($_SESSION['nickname'], 'Shift-JIS', 'UTF-8'), ENT_QUOTES,'Shift-JIS'); ?></u>さん</p>
         <ul>
             <li><a href="logout.php">ログアウト</a></li>
         </ul>
