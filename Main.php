4ef4255a9a3067c33bb88b48ad876a5a98581d54
長田 凌 <nagata@private.wide.co.jp>
2019-04-10 16:19:24 +0900

    カウント機能完成？

--- Main.php
+++ Main.php
@@ -19,7 +19,7 @@ if (!isset($_SESSION['nickname'])) {
     </head>
     <body>
         <h1>メイン画面</h1>
-        <p>ようこそ<u><?php echo htmlspecialchars($_SESSION['nickname'], ENT_QUOTES); ?></u>さん</p>
+        <p>ようこそ<u><?php echo htmlspecialchars($_SESSION['nickname'], ENT_QUOTES,'shift_JIS'); ?></u>さん</p>
         <ul>
             <li><a href="logout.php">ログアウト</a></li>
         </ul>
