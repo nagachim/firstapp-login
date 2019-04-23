5dbe23591a81b584226cda7d1cb4bf6939dc1b51
長田 凌 <nagata@private.wide.co.jp>
2019-04-23 10:11:25 +0900

    salesforceテスト

--- Main.php
+++ Main.php
@@ -41,7 +41,7 @@ $array = pg_fetch_result($result ,1 ,0);
     </head>
     <body>
         <h1>ÉÅÉCÉìâÊñ </h1>
-        <p>ÇÊÇ§Ç±Çª<u><?php echo htmlspecialchars($select, ENT_QUOTES,sjis); ?></u>Ç≥ÇÒ</p>
+        <p>ÇÊÇ§Ç±Çª<u><?php echo htmlspecialchars($connectString, ENT_QUOTES,sjis); ?></u>Ç≥ÇÒ</p>
         <div><?php
         if(!empty($result)){
         echo '<p>ÇÊÇ§Ç±Çªsalesforce <u><?php echo htmlspecialchars($array, ENT_QUOTES,sjis); ?></u>Ç≥ÇÒ</p>';
