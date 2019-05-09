596f409d1ab0f75e77be9d232c4e84fa5b419304
長田 凌 <nagata@private.wide.co.jp>
2019-05-09 11:16:09 +0900

    salesforce

--- Main.php
+++ Main.php
@@ -43,10 +43,10 @@ if(!$result = pg_connect($connectString)){
     <body>
         <h1>メイン画面</h1>
         <p>ようこそ<u><?php echo htmlspecialchars($str, ENT_QUOTES,sjis); ?></u>さん</p>
-        <div><?php
-        if(empty($result)){
+        <!-- <?php
+        if(empty($result)){ -->
         echo '<p>ようこそsalesforce <u><?php echo htmlspecialchars($name, ENT_QUOTES); ?></u>さん</p>';
-        }?></div>
+        <!-- }?> -->
         
         <ul>
             <li><a href="logout.php">ログアウト</a></li>
