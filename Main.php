d814d562debb704a25ea4f9746a611e9abf7bf95
長田 凌 <nagata@private.wide.co.jp>
2019-05-14 18:27:15 +0900

    メイン画面改修

--- Main.php
+++ Main.php
@@ -54,8 +54,9 @@ if(!$result = pg_connect($connectString)){
         <br>
         <label for="Add">住所：</label><input type="text" id="add" name="add" value="<?php echo htmlspecialchars($add, ENT_QUOTES,utf-8); ?>">
         <br>
+        <label for="Bikou">備考：</label>
         <br>
-        <label for="Bikou">備考：</label><textarea name="bikou" rows="4" cols="22"><?php echo htmlspecialchars($bikou, ENT_QUOTES,utf-8); ?></textarea>
+        <br><textarea name="bikou" rows="4" cols="22"><?php echo htmlspecialchars($bikou, ENT_QUOTES,utf-8); ?></textarea>
         <br>
         <br>
         <input type="submit" id="update" name="update" value="更新">
