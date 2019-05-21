882a1f1e37408421886c222fac7b6c28f3105bc1
長田 凌 <nagata@private.wide.co.jp>
2019-05-21 16:18:45 +0900

    css編集

--- Main.php
+++ Main.php
@@ -34,6 +34,11 @@ if(!$result = pg_connect($connectString)){
 	$age = $arr[12];
 }
 
+if(isset($_POST['update'])){
+	$update = sprintf("UPDATE salesforce.test__c SET testbikou__c=$_POST['bikou'],testsex__c=$_POST['sex'], testadd__c = $_POST['add'], testage__c = $_POST['age'] ");
+	$result = pg_query($update);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -44,30 +49,31 @@ if(!$result = pg_connect($connectString)){
         <link rel= "stylesheet" href="style.css">
     </head>
     <body>
-        <h1>メイン画面</h1>
+		<form action="Main.php" method="post">
+	        <h1>メイン画面</h1>
 
-        <p>ようこそ<u> salesforce会員 <?php echo htmlspecialchars($name, ENT_QUOTES,utf-8); ?></u>さん</p>
-        
-		<section>
-			<label for="Age">年齢：</label><input type="text" id="age" name="age" value="<?php echo htmlspecialchars($age, ENT_QUOTES,utf-8); ?>">
-		</section>
-        
-		<section>
-			<label for="Sex">性別：</label><input type="text" id="sex" name="sex" value="<?php echo htmlspecialchars($sex, ENT_QUOTES,utf-8); ?>">
-		</section>
-        
-		<section>
-			<label for="Add">住所：</label><input type="text" id="add" name="add" value="<?php echo htmlspecialchars($add, ENT_QUOTES,utf-8); ?>">
-		</section>
-		
-		<section class="bikou">
-			<label for="Bikou">備考：</label>
-				<textarea name="bikou" rows="4" cols="22" vertical-align:top><?php echo htmlspecialchars($bikou, ENT_QUOTES,utf-8); ?></textarea>
-		</section>
-        <br>
-        <input type="submit" id="update" name="update" value="更新">
-        <br>
-        <a href="logout.php">ログアウト</a>
-        
+	        <p>ようこそ<u> salesforce会員 <?php echo htmlspecialchars($name, ENT_QUOTES,utf-8); ?></u>さん</p>
+	        
+			<section>
+				<label for="Age">年齢：</label><input type="text" id="age" name="age" value="<?php echo htmlspecialchars($age, ENT_QUOTES,utf-8); ?>">
+			</section>
+	        
+			<section>
+				<label for="Sex">性別：</label><input type="text" id="sex" name="sex" value="<?php echo htmlspecialchars($sex, ENT_QUOTES,utf-8); ?>">
+			</section>
+	        
+			<section>
+				<label for="Add">住所：</label><input type="text" id="add" name="add" value="<?php echo htmlspecialchars($add, ENT_QUOTES,utf-8); ?>">
+			</section>
+			
+			<section class="bikou">
+				<label for="Bikou">備考：</label>
+					<textarea name="bikou" rows="4" cols="22" vertical-align:top><?php echo htmlspecialchars($bikou, ENT_QUOTES,utf-8); ?></textarea>
+			</section>
+	        <br>
+	        <input type="submit" id="update" name="update" value="更新">
+	        <br>
+	        <a href="logout.php">ログアウト</a>
+        </form>
     </body>
 </html>
